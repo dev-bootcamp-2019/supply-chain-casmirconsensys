5c42a426cd01d49aa4eb6b3cb8c7996ea2fbc80a
RaxxCoin <raxxcoin@gmail.com>
2019-01-26 19:44:46 -0800

    add bank

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity ^0.5.0;
 
 contract Migrations {
   address public owner;
@@ -21,4 +21,3 @@ contract Migrations {
     upgraded.setCompleted(last_completed_migration);
   }
 }
-
